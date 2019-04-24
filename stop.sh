48388adc1eb2df23d071211af117c74af6730428
lijiacaigit <1050518702@qq.com>
2019-04-24 07:16:32 +0000

    add stop

---
+++ stop.sh
@@ -0,0 +1,11 @@
+ps -ef|grep nike | awk '{print $2}' | xargs kill -9
+
+
+ps -ef|grep run_login.py | awk '{print $2}' | xargs kill -9
+ps -ef|grep run_address.py | awk '{print $2}' | xargs kill -9
+ps -ef|grep run_order.py | awk '{print $2}' | xargs kill -9
+ps -ef|grep run_register.py | awk '{print $2}' | xargs kill -9
+ps -ef|grep run_buy.py | awk '{print $2}' | xargs kill -9
+ps -ef|grep run_buy_and_order.py | awk '{print $2}' | xargs kill -9
+
+ps -ef|grep chrome | awk '{print $2}' | xargs kill -9
