32eaebd235bd972d002b47b1752ca5ff7d295d83
lijiacaigit <1050518702@qq.com>
2019-05-02 09:49:49 +0000

    mf

--- stop.sh
+++ stop.sh
@@ -7,5 +7,6 @@ ps -ef|grep run_order.py | awk '{print $2}' | xargs kill -9
 ps -ef|grep run_register.py | awk '{print $2}' | xargs kill -9
 ps -ef|grep run_buy.py | awk '{print $2}' | xargs kill -9
 ps -ef|grep run_buy_and_order.py | awk '{print $2}' | xargs kill -9
+ps -ef|grep buy_and_order | awk '{print $2}' | xargs kill -9
 
 ps -ef|grep chrome | awk '{print $2}' | xargs kill -9
